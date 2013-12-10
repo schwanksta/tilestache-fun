38ec99a478e7487dd1cca77b25ce6f4d56603020
Ken Schwencke <schwank@gmail.com>
2013-12-09 23:46:22 -0800

    make the bldg dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+mkdir -p data/buildings
 wget -O data/buildings/buildings.zip http://egis3.lacounty.gov/dataportal/wp-content/uploads/2012/11/lariac_buildings_2008.zip
 cd data/buildings && unzip buildings.zip
 cd ../../
