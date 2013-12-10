a423512ec11162bf0656259c41308e0385d7120d
Ken Schwencke <schwank@gmail.com>
2013-12-09 23:45:16 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Just run ./boostrap.sh, then pip install -r requirements.txt and run 
+./tilestache-server.py. Voila.
+
+Make sure you have environment variables set for your S3 config.
---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+wget -O data/buildings/buildings.zip http://egis3.lacounty.gov/dataportal/wp-content/uploads/2012/11/lariac_buildings_2008.zip
+cd data/buildings && unzip buildings.zip
+cd ../../
+cd data/countries && unzip countries.zip
